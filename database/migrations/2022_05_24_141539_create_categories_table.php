d1d9d3e2638247f711c8a9d1f363615f4040a283
Neeiser <stortini.study@gmail.com>
2022-05-24 17:13:48 +0200

    typo categories

--- database/migrations/2022_05_24_141539_create_categories_table.php
+++ database/migrations/2022_05_24_141539_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name');
-            $table->string('category_description')->nullable();
+            $table->string('categories_name');
+            $table->string('categories_description')->nullable();
         });
     }
 
